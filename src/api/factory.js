46bbbaa7b9f5596467677c09b2f8ed7c14b8b802
hotbreakb <areyhappy12@gmail.com>
2024-10-02 19:51:22 +0900

    Add : query factory

--- src/api/factory.js
+++ src/api/factory.js
@@ -1,8 +1,127 @@
-import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+import axiosInstance from "./instance";
 
-const api = axios.create({
-  baseURL: "https://api.example.com",
-});
+const signUp = async (data) => {
+  const response = await axiosInstance.post("/auth/sign-up", data);
+  return response.data;
+};
 
-export const loginApi = (credentials) => api.post("/login", credentials);
-export const fetchContentApi = () => api.get("/content");
+const reissueToken = async (memberId) => {
+  const response = await axiosInstance.post(
+    `/auth/reissue?memberId=${memberId}`
+  );
+  return response.data;
+};
+
+const clickRequest = async (memberId) => {
+  const response = await axiosInstance.post(`/click?memberId=${memberId}`);
+  return response.data;
+};
+
+const getClickResult = async (memberId) => {
+  const response = await axiosInstance.get(`/click?memberId=${memberId}`);
+  return response.data;
+};
+
+const enterChatRoom = async ({ senderId, chatRoomId }) => {
+  const response = await axiosInstance.get(
+    `/group-chat/enter?senderId=${senderId}&chatRoomId=${chatRoomId}`
+  );
+  return response.data;
+};
+
+const leaveChatRoom = async ({ senderId, chatRoomId }) => {
+  const response = await axiosInstance.get(
+    `/group-chat/leaver?senderId=${senderId}&chatRoomId=${chatRoomId}`
+  );
+  return response.data;
+};
+
+const getChatMessages = async ({ senderId, chatRoomId }) => {
+  const response = await axiosInstance.get(
+    `/group-chat/messages?senderId=${senderId}&chatRoomId=${chatRoomId}`
+  );
+  return response.data;
+};
+
+const receiveStompMessage = async (data) => {
+  const response = await axiosInstance.post("/group-chat/STOMP", data);
+  return response.data;
+};
+
+export const queryKeys = {
+  auth: {
+    signUp: {
+      mutationFn: signUp,
+    },
+    reissueToken: {
+      mutationFn: reissueToken,
+    },
+  },
+  click: {
+    request: {
+      mutationFn: clickRequest,
+    },
+    result: (memberId) => ({
+      queryKey: ["clickResult", memberId],
+      queryFn: () => getClickResult(memberId),
+    }),
+  },
+  chat: {
+    enter: {
+      mutationFn: enterChatRoom,
+    },
+    leave: {
+      mutationFn: leaveChatRoom,
+    },
+    messages: ({ senderId, chatRoomId }) => ({
+      queryKey: ["chatMessages", senderId, chatRoomId],
+      queryFn: () => getChatMessages({ senderId, chatRoomId }),
+    }),
+    stomp: {
+      mutationFn: receiveStompMessage,
+    },
+  },
+};
+
+export const useSignUp = () => useMutation(queryKeys.auth.signUp.mutationFn);
+export const useReissueToken = () =>
+  useMutation(queryKeys.auth.reissueToken.mutationFn);
+export const useClickRequest = () =>
+  useMutation(queryKeys.click.request.mutationFn);
+export const useGetClickResult = (memberId) =>
+  useQuery(queryKeys.click.result(memberId));
+export const useEnterChatRoom = () =>
+  useMutation(queryKeys.chat.enter.mutationFn);
+export const useLeaveChatRoom = () =>
+  useMutation(queryKeys.chat.leave.mutationFn);
+export const useGetChatMessages = ({ senderId, chatRoomId }) =>
+  useQuery(queryKeys.chat.messages({ senderId, chatRoomId }));
+export const useReceiveStompMessage = () =>
+  useMutation(queryKeys.chat.stomp.mutationFn);
+
+// export const TokenResponsePropTypes = PropTypes.shape({
+//   accessToken: PropTypes.string.isRequired,
+//   refreshToken: PropTypes.string.isRequired,
+// });
+
+// export const ErrorResponsePropTypes = PropTypes.shape({
+//   exceptionCode: PropTypes.number.isRequired,
+//   message: PropTypes.string.isRequired,
+// });
+
+// export const ClickResultPropTypes = PropTypes.shape({
+//   count: PropTypes.string.isRequired,
+//   clickRank: PropTypes.objectOf(PropTypes.string).isRequired,
+// });
+
+// export const ChatMessagePropTypes = PropTypes.shape({
+//   id: PropTypes.string.isRequired,
+//   senderNickname: PropTypes.string.isRequired,
+//   content: PropTypes.string.isRequired,
+//   createdAt: PropTypes.string.isRequired,
+// });
+
+// export const ChatMessagesResponsePropTypes = PropTypes.shape({
+//   groupChatMessageResponses: PropTypes.arrayOf(ChatMessagePropTypes).isRequired,
+// });
